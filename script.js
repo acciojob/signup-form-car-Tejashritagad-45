c4357d170619cb5a08b78d3ce5fd775c21e7aef4
Tejashri Tagad <tagadteju45@gmail.com>
2025-10-10 21:09:17 +0530

    created a submission

--- script.js
+++
@@ -1,2 +0,0 @@
-//your code herehttps://www.svgrepo.com/show/345221/three-dots.svg
-https://www.svgrepo.com/show/345221/three-dots.svghttps://www.svgrepo.com/show/345221/three-dots.svghttps://www.svgrepo.com/show/345221/three-dots.svghttps://www.svgrepo.com/show/345221/three-dots.svg
